4403640b9dedfeab5e0916406c58ff3d59feeaa7
lenny <lenny.kimanthi@student.moringaschool.com>
2026-02-10 18:46:33 +0300

    Update profile images and fix typing animation layout

---
+++ process_image.py
@@ -0,0 +1,8 @@
+from PIL import Image
+import os
+
+# Change to the img directory
+os.chdir('/home/webster/Desktop/lenny\'s-portfolio/assets/img')
+
+# Create a placeholder for now - you'll need to save your image as 'original.png' first
+print("Ready to process image")
